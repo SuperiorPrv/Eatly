6b6cfcf4210e132fdd8c46d3dd415a16007f663e
Superior_P <superior.prv@gmail.com>
2024-09-22 19:15:45 +0500

    menu finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { lazy, Suspense } from "react"
 import Layout from "./layout/Layout"
 import Home from "./pages/home/Home"
-import Menu1 from "./pages/menu/menu1/Menu1"
 import Blog from "./pages/blog/Blog"
 import Pricing from "./pages/pricing/Pricing"
 import Contact from "./pages/contact/Contact"
 import Menu2 from "./pages/menu/menu2/Menu2"
 import Menu3 from "./pages/menu/menu3/Menu3"
+import Loading from "./components/shared/Loading/Loading"
+
+let Menu1 = lazy(()=>import('./pages/menu/menu1/Menu1'))
 
 
 const App = () => {
@@ -25,7 +28,9 @@ const App = () => {
           },
           {
             path:"/menu",
-            element:<Menu1/>
+            element:<Suspense fallback={<Loading/>}>
+              <Menu1/>
+            </Suspense>
           },
           {
             path:"/menu2",
