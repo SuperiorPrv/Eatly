273702d6fd59e87fb03a3fda0043fac171d0d351
Superior_P <superior.prv@gmail.com>
2024-09-22 13:24:28 +0500

    add other menus

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Container from '../Container/Container'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Container>
+      <footer>
+        <div>
+
+        </div>
+        <hr />
+        <div>
+          <p></p>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" /></div>
+        </div>
+      </footer>
+    </Container>
   )
 }
 
