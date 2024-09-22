5bf5d602374ab30da89e54dd5fce58b7495e0a6d
Superior_P <superior.prv@gmail.com>
2024-09-22 13:32:11 +0500

    add other pages

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Container from '../Container/Container'
+import logo from "../Header/img/Logo.png";
 
 const Footer = () => {
   return (
     <Container>
       <footer>
         <div>
-
+          <img src={logo} alt="" />
+          <p></p>
+          <p></p>
+          <p></p>
+          <p></p>
         </div>
         <hr />
         <div>
