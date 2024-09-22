be14b55a3eaa797ef103b9e9bfec3de6f6d64b6b
Muhammad <muhammad20080516@gmail.com>
2024-09-22 13:43:17 +0500

    fix

--- src/pages/menu/menu2/Menu2.jsx
+++ src/pages/menu/menu2/Menu2.jsx
@@ -1,8 +1,11 @@
+import Container from "@/components/shared/Container/Container"
 import img from "./img/Mask Group (1).png"
 
 const Menu2 = () => {
   return <>
+    <Container>
     <img src={img} alt="" />
+  </Container>
   </>
 }
 
