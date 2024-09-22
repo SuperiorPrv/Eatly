a2eded4b97f049d1c44e132763a24b4bf79d0a1b
ismoil <nasridinovismoil945@gmail.com>
2024-09-22 17:45:35 +0500

    page pricing section1 add

--- src/components/componentsPageHome/section1/Section1.jsx
+++ src/components/componentsPageHome/section1/Section1.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Section1 = () => {
   return (
-    <div></div>
+   <div className="h-[700px]">
+     <div  className='bg-[#5C4EAE] h-[237px] items-center '   >
+        <div className="flex  gap-[10px] font-[600] text-[29px] m-auto w-[260px] relative top-[30px] h-[55px] text-white">
+            <img src="/src/pages/pricing/img/Vector.png" alt="" />
+            <h1>Our Pricing</h1>
+        </div>
+       <div className="block m-auto">
+       <img className='block m-auto' src="/src/pages/pricing/img/17-Home V1 1.png" alt="" />
+       </div>
+    </div><br />
+   </div>
   )
 }
 
---
+++ src/components/componentsPageHome/section2/Section2.jsx
@@ -0,0 +1,111 @@
+import React from "react";
+
+const Section3 = () => {
+  return (
+    <div className="max-w-3xl mx-auto p-8 bg-white rounded-lg  drop-shadow-lg overflow-auto  ">
+      <div className=" flex justify-between mb-[20px] min-w-[709px]">
+        <div className="">
+          <h1 className="font-[700] text-[29px]">Pricing</h1>
+          <p className="text-[#6E757C]">
+            Affordable Basic <br /> & Pro Plans
+          </p>
+        </div>
+        <div className="ml-[30px]">
+          <h1 className="font-[700] text-[29px]">Basic</h1>
+          <p className="text-[#6E757C]">
+            Completely 100% <br /> Free Plan
+          </p>
+        </div>
+        <div className="ml-[30px]">
+          <h1 className="font-[700] text-[29px]">Premium</h1>
+          <p className="text-[#6E757C]">
+            Amazing Premium <br /> Features Plan
+          </p>
+        </div>
+      </div>
+      <br />
+      <hr className="min-w-[709px]" />
+      <br />
+
+      <div className=" flex justify-between min-w-[709px]   text-start mb-[20px] text-[#606060] text-[16px]">
+        <div className=""></div>
+        <div className="">
+          <h1 className="font-[700] text-[29px]">
+            <span className="text-[14px] relative top-[-10px]">$</span> 0{" "}
+            <span className="text-[#6E757C] text-[11px]">/month</span>
+          </h1>
+        </div>
+        <div className="">
+          <h1 className="font-[700] text-[29px]">
+            <span className="text-[14px] relative top-[-10px]">$</span> 5{" "}
+            <span className="text-[#6E757C] text-[11px]">/month</span>
+          </h1>
+        </div>
+      </div>
+      <hr className="min-w-[709px]" />
+      <br />
+
+
+      <div className="">
+       <ul className="flex flex-wrap gap-[20px] min-w-[709px] items-center justify-between mb-[20px] text-[#606060] text-[16px]"> 
+        <li>Support 24/7</li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+       </ul>
+
+       <ul className="flex items-center min-w-[709px] justify-between mb-[20px] text-[#606060] text-[16px]"> 
+        <li>Fast Delivery</li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+       </ul>
+
+       <ul className="flex items-center min-w-[709px] justify-between mb-[20px] text-[#606060] text-[16px]"> 
+        <li>20% Off Food Deals</li>
+        <li className="ml-[-46px]"><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+       </ul>
+
+       <ul className="flex items-center min-w-[709px] justify-between mb-[20px] text-[#606060] text-[16px]"> 
+        <li>Transaction History</li>
+        <li className="ml-[-49px]"><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+       </ul>
+
+       <ul className="flex items-center min-w-[709px] justify-between mb-[20px] text-[#606060] text-[16px]"> 
+        <li>Weekend Deals</li>
+        <li className="ml-[-20px]"><img className="w-[20px]" src="/src/pages/pricing/img/image.png" alt="" /></li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+       </ul>
+
+
+       <ul className="flex items-center min-w-[709px] justify-between mb-[20px] text-[#606060] text-[16px]"> 
+        <li>Dashboard Access</li>
+        <li className="ml-[-48px]"><img className="w-[20px]" src="/src/pages/pricing/img/image.png" alt="" /></li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+       </ul>
+
+
+       <ul className="flex items-center min-w-[709px] justify-between mb-[20px] text-[#606060] text-[16px]"> 
+        <li>Premium Group Access</li>
+        <li className="mr-[76px]"><img className="w-[20px]" src="/src/pages/pricing/img/image.png" alt="" /></li>
+        <li><img className="w-[20px]" src="/src/pages/pricing/img/Tick.png" alt="" /></li>
+       </ul>
+
+
+      </div><br /><hr className="min-w-[709px]" /><br />
+
+      <div className="flex justify-around min-w-[709px]">
+        <button className="px-6 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 transition">
+          Start Free
+        </button>
+        <button className="px-6 py-2 bg-purple-600 text-white rounded-md hover:bg-purple-700 transition">
+          Start Pro
+        </button>
+      </div>
+      
+    </div>
+
+  );
+};
+
+export default Section3;
