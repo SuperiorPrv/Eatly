f8f70243a48a62c37f459b3ff6b7f95ca0dc85f6
Masrur <mansurovmasrur98@gmail.com>
2024-09-24 17:12:15 +0500

    animation HOME page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { lazy, Suspense } from "react"
 import Layout from "./layout/Layout"
-import Blog from "./pages/blog/Blog"
-import Home from "./pages/home/Home"
-import Pricing from "./pages/pricing/Pricing"
 import Contact from "./pages/contact/Contact"
 import Menu2 from "./pages/menu/menu2/Menu2"
 import SignUp from "./pages/sign-up/SignUp"
@@ -15,9 +12,6 @@ let Menu1 = lazy(()=>import('./pages/menu/menu1/Menu1'))
 let Menu3 = lazy(()=>import('./pages/menu/menu3/Menu3'))
 let Home = lazy(()=>import('./pages/home/Home'))
 let Pricing = lazy(()=>import('./pages/pricing/Pricing'))
-
-
-
 let Blog = lazy(()=>import ("./pages/blog/Blog"))
 
 const App = () => {
